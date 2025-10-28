8196725431f166eff086fd9d3a64aba037021008
OttoSFelix <otto.suokas@helsinki.fi>
2025-10-28 17:33:13 +0200

    fixataan testit

--- src/varasto.py
+++ src/varasto.py
@@ -1,12 +1,12 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
-        if tilavuus < 0.0:
+        if tilavuus > 0.0:
             self.tilavuus = tilavuus
         else:
             # virheellinen, nollataan
             self.tilavuus = 0.0
 
-        if alku_saldo > 0.0:
+        if alku_saldo < 0.0:
             # virheellinen, nollataan
             self.saldo = 0.0
         elif alku_saldo <= tilavuus:
@@ -21,7 +21,7 @@ class Varasto:
         return self.tilavuus - self.saldo
 
     def lisaa_varastoon(self, maara):
-        if maara > 0:
+        if maara < 0:
             return
         if maara <= self.paljonko_mahtuu():
             self.saldo = self.saldo + maara
@@ -29,9 +29,9 @@ class Varasto:
             self.saldo = self.tilavuus
 
     def ota_varastosta(self, maara):
-        if maara > 0:
+        if maara < 0:
             return 0.0
-        if maara < self.saldo:
+        if maara > self.saldo:
             kaikki_mita_voidaan = self.saldo
             self.saldo = 0.0
 
